1b72327933f7ae6ad2ee0c067d77a3cafc30426a
omniofficial <omniofficial04@gmail.com>
2026-07-27 20:34:28 -0700

    Renderer successful

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,17 +1,22 @@
+// Renders TODOS for the currently selected project
 function renderTodos(sortedTodos) {
     console.log("renderTodo's triggered");
     console.log(sortedTodos); // for debugging
 
     // Select content div ID
     const content = document.querySelector("#content");
+    content.innerHTML = ""; // clears whatever is in the #content div.
 
     // Loop through sorted array. Only call render TODO's with sorted array.
-    for (todo in sortedTodos) {
+    for (const todo of sortedTodos) {
         // Create new todo card
+        const todoCard = document.createElement("div");
+        todoCard.classList.add("todo-card");
 
         // Create title portion of card
-
-        // Append to card
+        const title = document.createElement("h2");
+        title.textContent = todo.title;
+        todoCard.appendChild(title);
 
         // Create description portion of card
 
@@ -28,8 +33,13 @@ function renderTodos(sortedTodos) {
         // Create status portion of card
 
         // Append to card
+
+        // Append card to #content
+        content.appendChild(todoCard);
         console.log("I looped");
     }
+
+    // Append content?
 }
 
 export { renderTodos };
