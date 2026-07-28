470ad802b6c659d498023332e7f3447ad82d73da
omniofficial <omniofficial04@gmail.com>
2026-07-27 20:03:38 -0700

    Reformatted content

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 TODO:
+Data mapping. Add projects functinality, each todo belongs to a specified project.
+
 Render page function. do a for loop. Maybe a rendering module.
 
 RENDER.js:
--- src/modules/render.js
+++ src/modules/render.js
@@ -2,7 +2,12 @@ function renderTodos(sortedTodos) {
     console.log("renderTodo's triggered");
     console.log(sortedTodos); // for debugging
 
+
+    // Select content 
+
     // Loop through sorted array. Only call render TODO's with sorted array.
+
+    
 }
 
 export { renderTodos };
